5da1827039549d6a97cd37d8a56926894aedf880
vole <wu1424858317@qq.com>
2017-06-13 22:57:46 +0800

    add fireabse

--- app/src/main/java/cn/studyjams/s2/a0123/wujiweather/MainActivity.java
+++ app/src/main/java/cn/studyjams/s2/a0123/wujiweather/MainActivity.java
@@ -2,16 +2,20 @@ package cn.studyjams.s2.a0123.wujiweather;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         if(preferences.getString("weather",null)!=null){
             Intent intent = new Intent(this,WeatherActivity.class);
